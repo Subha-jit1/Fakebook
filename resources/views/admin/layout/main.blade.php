37a52fe5726aec4799abb6693f8e91f015c1a57e
devsubha99 <devsubha31@gmail.com>
2024-03-17 20:13:05 +0530

    created admin layout

---
+++ resources/views/admin/layout/main.blade.php
@@ -0,0 +1,6 @@
+@include('admin.layout.head')
+@include('admin.layout.sidebar')
+@include('admin.layout.nav')
+@yield('content')
+@include('admin.layout.footer')
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\indexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[indexController::class, 'index'])->name('index');
